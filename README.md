1ded370eeede28c63dddd119910f4fa37a0af2ea
Haru Mekki <mekkiharukaze@gmail.com>
2021-03-21 03:55:16 +0900

    :construction: Message Moderation ( Delete )
    - Added validation to check if sender is self.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # saionji-kanaria
 A Discord Bot with mish-mash of stuff !
+
+## Current Goals
+1. Content Moderation
+2. Direct Message interactions
+3. Pull game news with a command
--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import Discord from 'discord.js';
-import { PARTIALS } from '../utils/constants/discord-constants';
+import { PARTIALS } from '../utils/constants/discord-constants.js';
 
 const app = async () => {
-  const discordClient = new Discord.Client({ partials: PARTIALS});
+  const discordClient = new Discord.Client({ partials: PARTIALS });
+  const loginToken = process.env.SAIONJI_TOKEN;
+  const ownId = process.env.SAIONJI_KANARIA_ID;
 
   discordClient.on('ready', () => {
     console.log('Saionji-Kanaria is Ready !');
   });
 
   discordClient.on('message', (msg) => {
-    if (msg.content === 'Hallo') {
-      msg.channel.send('Nyahallo!');
+    if (msg.author.id !== ownId) {
+      if (msg.content.toLowerCase().includes('hallo')) {
+        msg.channel.send('Nyahallo!');
+      }
     }
   })
 
-  await discordClient.login(process.env.SAIONJI_TOKEN);
+  await discordClient.login(loginToken);
 };
 
 export default app;
