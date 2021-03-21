e3876f7b91b5d41009495eb412639cba53aeea7f
Haru Mekki <mekkiharukaze@gmail.com>
2021-03-21 20:49:20 +0900

    :art: fixed indention and stuff

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import fs from 'fs';
 import Discord from 'discord.js';
-import { PARTIALS } from './utils/constants/discord-constants.js';
+import {PARTIALS} from './utils/constants/discord-constants.js';
 import app from './src/app.js';
 
 config();
-const discordClient = new Discord.Client({ partials: PARTIALS });
+const discordClient = new Discord.Client({partials: PARTIALS});
 const loginToken = process.env.SAIONJI_TOKEN;
 
 discordClient.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('src/commands');
 
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`src/commands/${folder}`).filter(file => file.endsWith('.js'));
+  const commandFiles = fs.readdirSync(`src/commands/${folder}`).filter(file => file.endsWith('.js'));
 
-	for (const file of commandFiles) {
-		const { default: command } = await import(`./src/commands/${folder}/${file}`);
+  for (const file of commandFiles) {
+    const {default: command} = await import(`./src/commands/${folder}/${file}`);
 
-		if (typeof command.name === 'object') {
-			for (const alternateCommand of command.name) {
-				discordClient.commands.set(alternateCommand, command);
-			}
-		} else {
-			discordClient.commands.set(command.name, command);
-		}
-	}
+    if (typeof command.name === 'object') {
+      for (const alternateCommand of command.name) {
+        discordClient.commands.set(alternateCommand, command);
+      }
+    } else {
+      discordClient.commands.set(command.name, command);
+    }
+  }
 }
 
 discordClient.on('ready', () => {
-	console.log('Saionji-Kanaria is Ready !');
+  console.log('Saionji-Kanaria is Ready !');
 });
 
 await discordClient.login(loginToken);
--- src/commands/moderation/clean.js
+++ src/commands/moderation/clean.js
@@ -1,26 +1,26 @@
 const cleanMessages = (message, args) => {
-	const amountToClean = parseInt(args[0]);
+  const amountToClean = parseInt(args[0]);
 
-	if (isNaN(amountToClean)) {
-		return message.channel.send("Kana needs numbers!");
-	}
+  if (isNaN(amountToClean)) {
+    return message.channel.send("Kana needs numbers!");
+  }
 
-	if (amountToClean < 2 || amountToClean > 100) {
-		return message.channel.send("P-please provide an amount of messages to be deleted between 2 and 100!");
-	}
+  if (amountToClean < 2 || amountToClean > 100) {
+    return message.channel.send("P-please provide an amount of messages to be deleted between 2 and 100!");
+  }
 
-	message.channel.bulkDelete(amountToClean)
-		.then(messages => {
-			message.channel.send(`Kana has successfully cleaned ${messages.size} messages!`);
-		})
-		.catch(err => {
-			console.error(err);
-			message.channel.send("Kana couldn't clean the channel QwQ");
-		});
+  message.channel.bulkDelete(amountToClean)
+  .then(messages => {
+    message.channel.send(`Kana has successfully cleaned ${messages.size} messages!`);
+  })
+  .catch(err => {
+    console.error(err);
+    message.channel.send("Kana couldn't clean the channel QwQ");
+  });
 };
 
 export default {
-	name: 'clean',
-	description: 'Removes messages in bulk or individually',
-	execute: cleanMessages
+  name: 'clean',
+  description: 'Removes messages in bulk or individually',
+  execute: cleanMessages
 };
